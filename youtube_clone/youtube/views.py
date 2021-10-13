b03a04411e02755bdaff6d2b1719769230476e50
stevenpfaff <pfaffy1426@gmail.com>
2021-10-13 16:38:07 -0500

    finshed backend API

--- youtube_clone/youtube/views.py
+++ youtube_clone/youtube/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from .models import Comment
+from .serializers import CommentSerializer
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
 
-# Create your views here.
+class CommentList(APIView):
+    def get(self, request):
+        comment = Comment.objects.all()
+        serializer = CommentSerializer(comment, many = True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = CommentSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status = status.HTTP_201_CREATED)
+        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
