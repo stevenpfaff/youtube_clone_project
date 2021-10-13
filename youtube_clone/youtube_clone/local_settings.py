5202be598c20b9079cf4deedcf91f18a4647c00c
stevenpfaff <pfaffy1426@gmail.com>
2021-10-13 15:50:45 -0500

    initial commit

---
+++ youtube_clone/youtube_clone/local_settings.py
@@ -0,0 +1,18 @@
+
+
+
+SECRET_KEY = 'django-insecure-)wmx@n=r_sk%_ysohh191r%rb(=!8e!)&$-4%e#t4$50_*=q3p'
+
+DATABASES = {
+    'default': {
+    "ENGINE" : "mysql.connector.django",
+    "NAME" : "youtube_clone",
+    "USER" : "root",
+    "PASSWORD" : "stammer91",
+    "HOST" : "127.0.0.1",
+    "PORT" : "3306",
+    "OPTIONS" : {
+        "autocommit" : True
+    }
+}
+}
