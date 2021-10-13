3a621c78a319dd01aae8b3e034bf8fd70fc5044a
stevenpfaff <pfaffy1426@gmail.com>
2021-10-13 16:04:37 -0500

    models initialized

--- youtube_clone/youtube/models.py
+++ youtube_clone/youtube/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class Comment(models.Model): 
+    comment = models.CharField(max_length=300)
+    response = models.CharField(max_length=300)
